452cd97a5459dfe5896b849209230f08a9508efa
Alessio Colucci <alessio.colucci@gmail.com>
2019-12-03 22:37:06 +0100

    Initial definitions for plot metrics

--- src/weight_plotting/weight_loss_coordinates.py
+++ src/weight_plotting/weight_loss_coordinates.py
@@ -6,13 +6,29 @@ import pprint
 import sys
 
 import torch
-import torch.optim as optim
-from torch.backends import cudnn
-from torch.autograd import Variable
-import torchvision.utils as vutils
-from tqdm import tqdm
-import torch.autograd.profiler
 
 PROJECT_DIR = pathlib.Path(__file__).absolute().parent.parent # main directory, the parent of src
 if str(PROJECT_DIR) not in sys.path:
     sys.path.append(str(PROJECT_DIR))
+
+DEFAULT_METRIC = torch.nn.MSELoss(reduction='sum')
+DEFAULT_POSITIVE_MARGIN = 0.05
+DEFAULT_NEGATIVE_MARGIN = -0.05
+
+def compute_weight_metric(weights, target=None, metric=DEFAULT_METRIC):
+    if target is None:
+        target = torch.zeros(weights.size())
+    return metric(weights, target)
+
+def compute_model_metric(parameters, targets=None, metric=DEFAULT_METRIC):
+    if targets is None:
+        targets = [None] * len(list(weights))
+        final_target = None
+    else:
+        final_target = targets
+    return compute_weight_metric(torch.tensor(list(compute_weight_metric(w, t, metric) for w, t in zip(weights, targets))), final_target, metric)
+
+def compute_landscape_x(parameters, pos_margin=DEFAULT_POS_MARGIN):
+    pass
+
+
